ee98a19af137f0712d3c5cb759108d09d890d29f
niklas <niklas.brynolf@gmail.com>
2018-05-15 22:57:34 +0200

    added routing, db model and docker compose

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = { 
-    url: 'mongodb://mongo/knittingDB'
+    url: 'mongodb://database/knittingDB'
 };
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,13 +1,65 @@
 /*import express from "express";
 const router =  express.router();*/
-
+const mongoose  = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+ 
+
+// mongodb url from docker compose
+const dbHost = 'mongodb://database/knittingDB';
+
+// connect to monodb
+mongoose.connect(dbHost);
+
+// mongoise schema
+const pictureSchema = new mongoose.Schema({
+    pictureName: String,
+    pictureDescription: String
+});
+
+// create mongoose model
+const Picture = mongoose.model('Picture', pictureSchema);
+
 // get apt listing
 
-router.get('/', ( req, res ) => {
+router.get( '/', ( req, res ) => {
     res.send('api works');
+});
+
+// get all pictures 
+router.get( '/pictures', ( req, res ) => {
+    Picture.find({}, ( err, pictures ) => {
+        if( err ) res.status(500).send(error);
+
+        res.status(200).json(pictures);
+
+    });
+});
+
+// get one picture
+router.get( '/pictures:id', ( req, res ) => {
+    Picture.findById(req.param.id, (err, pictures) => {
+        if(err) res.status(500).send(error);
+
+        res.status(200).json(pictures);
+    }); 
+});
+
+// create picture name and description 
+router.post( 'pictures', ( req, res ) => {
+    let picture = new Picture({
+        pictureName: req.body.pictureName,
+        pictureDescription: req.body.pictureDescription
+    });
+
+    picture.save( error => {
+        if(error) res.status(500).send(error);
+
+        res.status(201).json({
+            message: 'picture uploaded seuccessfully'
+        })
+    });
 });
 
 // export default router;
